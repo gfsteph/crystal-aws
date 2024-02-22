e54ad6e8be882eefb65c439e516ebe33363d428c
Stephane Paul <stephane.paul@greenfence.com>
2024-02-22 14:49:06 -0500

    chore: make it configurable

--- src/client.cr
+++ src/client.cr
@@ -17,8 +17,19 @@ module AWS
       @access_key_id = AWS.access_key_id,
       @secret_access_key = AWS.secret_access_key,
       @region = AWS.region,
-      @endpoint = URI.parse("https://#{service_name}.#{region}.amazonaws.com"),
+      endpoint_url : String? = nil
     )
+      endpoint_scheme = "https" # Default to HTTPS for security
+      if endpoint_url
+        parsed_endpoint = URI.parse(endpoint_url)
+        endpoint_scheme = parsed_endpoint.scheme
+        endpoint_host = parsed_endpoint.host.not_nil!
+      else
+        endpoint_host = "#{service_name}.#{region}.amazonaws.com"
+      end
+      @endpoint = URI.parse("#{endpoint_scheme}://#{endpoint_host}")
+
+      puts "Listing on #{@endpoint}"
       @signer = Awscr::Signer::Signers::V4.new(service_name, region, access_key_id, secret_access_key)
       @connection_pools = Hash({String, Bool}, DB::Pool(HTTP::Client)).new
     end
@@ -27,45 +38,15 @@ module AWS
       "Connection" => "keep-alive",
       "User-Agent" => "Crystal AWS #{VERSION}",
     }
-    def get(path : String, headers = HTTP::Headers.new)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.get(path, headers: headers))
-    end
-
-    def get(path : String, headers = HTTP::Headers.new, &block : HTTP::Client::Response ->)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.get(path, headers: headers, &block))
-    end
-
-    def post(path : String, body : String, headers = HTTP::Headers.new)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.post(path, body: body, headers: headers))
-    end
-
-    def put(path : String, body : IO, headers = HTTP::Headers.new)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.put(path, body: body, headers: headers))
-    end
-
-    def head(path : String, headers : HTTP::Headers)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.head(path, headers))
-    end
-
-    def delete(path : String, headers = HTTP::Headers.new)
-      headers = DEFAULT_HEADERS.dup.merge!(headers)
-      http(&.delete(path, headers: headers))
-    end
 
     protected getter endpoint
 
-    protected def http(host = endpoint.host.not_nil!, tls = true)
+    protected def http(host = endpoint.host.not_nil!, tls = endpoint.scheme == "https")
       pool = @connection_pools.fetch({host, tls}) do |key|
         @connection_pools[key] = DB::Pool.new(initial_pool_size: 0, max_idle_pool_size: 20) do
           http = HTTP::Client.new(host, tls: tls)
           http.before_request do |request|
-            # Apparently Connection: keep-alive causes trouble with signatures.
-            # See https://github.com/taylorfinnell/awscr-signer/issues/56#issue-801172534
+            # Sign request
             request.headers.delete "Authorization"
             request.headers.delete "X-Amz-Content-Sha256"
             request.headers.delete "X-Amz-Date"
